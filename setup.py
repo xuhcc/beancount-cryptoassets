471d42d812744171dedf52d6bd22f2655bdf17b7
Kirill Goncharov <kdgoncharov@gmail.com>
2019-08-22 12:26:41 +0300

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='beancount-cryptoassets',
-    version='1.1.0',
+    version='1.1.1',
     description='Beancount Cryptoassets',
     packages=['beancount_cryptoassets'],
     license='GPLv3',
